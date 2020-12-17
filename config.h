8000d50351bc89bf419a38d3727236e667d6ae38
potatoalienof13 <potato@copao13.attlocal.net>
2020-12-17 10:22:57 -0500

    Changed the default config

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 #include "stdbool.h"
 #include "structs.h"
-const int surf_wid = 5720;
+const int surf_wid = 1920;
 const int surf_hei = 1080;
-int num_shapes = 100000;
-int num_points = 100;
+int num_shapes = 20000;
+int num_points = 10;
 
 
 
-const int circleSize = 50;
+const int circleSize = 20;
 double circleOpacity = 1;
 
 
@@ -16,39 +16,37 @@ double VSCmin = 10;
 
 int numColors;
 enum colorMethods {def, tintPoint};
-int colorMethod = tintPoint;
+int colorMethod = def;
 color tintColor = {0,0,1};
-double tintIntens = 0.2;
+double tintIntens = 0.5;
 
 enum pointMethods {randPoints, gridPoints};
 int pointMethod = gridPoints;
-XY gridMargs = {0, 0};
-XY gridDims = {240,250};
+XY gridMargs = {60, 40};
+XY gridDims = {300,250};
 
 
 enum methods {onePoint,imaginePoint, twoPoints, twoPointsRP, twoPointsNG, twoPointsVSC, pointRange, pointBank};
 int method = twoPoints;
-double pointBankZeroDist = 1000;
+
+color imagineColor = {1,0,0};
+double imagineDist =  250;
+
+double pointBankZeroDist = 50;
 double allPointPow = 40;
 
-color imagineColor = {0.7,0.1,0.1};
-double imagineDist =  10;
-
-bool pixel = true;
+bool pixel = false;
 XY recDims = {1,1};
 
-
 bool debug = false;
-
-
 
 enum colorProcessing {normal, crazyColors, squareMod, limits};
 int colorProc = normal;
 
-color upperColLim = {1,1,0.7};
-color lowerColLim = {0,0.3,0};
+color upperColLim = {1,1,1};
+color lowerColLim = {0,0,0};
 
 double crazyColorsMult = 1.5;
 double colorExpo = 2;
 
-char fileName[] = "hahahfilename.png";
+char fileName[] = "pic.png";
